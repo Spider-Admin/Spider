fff5fbb4f37f3c2d6cce0ae5a47f63bf6110a445
Spider-Admin <Spider-Admin@proton.me>
2024-10-04 21:14:44 +0200

    Use Freemarker 2.3.33 for output

--- src/main/java/org/spider/Output.java
+++ src/main/java/org/spider/Output.java
@@ -102,7 +102,7 @@ public class Output implements AutoCloseable {
 		this.storage = new Storage(connection);
 		this.settings = Settings.getInstance();
 
-		templateConfig = new Configuration(Configuration.VERSION_2_3_30);
+		templateConfig = new Configuration(Configuration.VERSION_2_3_33);
 		templateConfig.setDefaultEncoding(settings.getCharset().name());
 		templateConfig.setLocale(Locale.US);
 		templateConfig.setDateTimeFormat("yyyy-MM-dd HH:mm:ss");
